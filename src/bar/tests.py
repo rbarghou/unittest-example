0d9df9ca3c79f89e53a9b57943ed0db1c09da544
ramsey <ramsey@addgene.org>
2016-02-15 13:00:12 -0500

    removed unnecessary files added content to readme modified test to provide additional example

--- src/bar/tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class BarTest(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def test_bar(self):
-        pass
--- src/foo/tests.py
+++ src/foo/tests.py
@@ -5,6 +5,12 @@ from .utils import standard_deviation
 
 class StandardDeviationTest(unittest.TestCase):
 
+    def test_simple(self):
+        self.assertEqual(
+            standard_deviation([1,2,3]),
+            0.816496580927726
+        )
+
     def test_against_scipy(self):
         for t in range(10):
             n = scipy.random.randint(10, 100)
